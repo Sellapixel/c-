b24564d09acc81adb04607b8a26a5a9df3617a8f
Sella Alessandro <alessandro.sella@itc_ceccato.loc>
2024-11-20 13:01:49 +0100

    Aggiunto il .gitignore

---
+++ Librerie/BaseStructs.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Anagrafe{
+	string nome;
+	string cognome;
+};
+struct data{
+	int giorno;
+	int mese;
+	int anno;
+};
+struct tempo{
+	int ore;
+	int min;
+	int sec;
+};
